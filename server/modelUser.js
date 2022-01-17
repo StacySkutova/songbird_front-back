c8792d66aaa50d9d3b1a538dbbd50f7b8d023234
StacySkutova <92952135+StacySkutova@users.noreply.github.com>
2022-01-18 01:52:00 +0300

    Delete server directory

--- server/modelUser.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require("mongoose");
-
-const User = new Schema({
-    username: {type: String, unique: true, require: true},
-    email: {type: String, unique: true, require: true},
-    password: {type: String, require: true},
-})
-
-module.exports = model("User", User);
