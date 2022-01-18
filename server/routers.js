e21cff200415151b69afa21156688509f352e6a6
StacySkutova <92952135+StacySkutova@users.noreply.github.com>
2022-01-18 20:48:50 +0300

    final front&back (reg,log,modals)

---
+++ server/routers.js
@@ -0,0 +1,28 @@
+const Router = require("express");
+const controller = require("./controllers");
+const { check } = require("express-validator");
+
+const router = new Router();
+
+router.post(
+  "/registration",
+  [
+    check("username", "Username field can not be empty").notEmpty(),
+    check("email", "Email field can not be empty").notEmpty().isEmail(),
+    check(
+      "password",
+      "Password field should be at least 5 characters and not more than 10"
+    ).isLength({ min: 5, max: 10 }),
+  ],
+  controller.registration
+);
+router.post(
+  "/login",
+  [
+    check("email", "Email field can not be empty").notEmpty().isEmail(),
+    check("password", "Password field can not be empty").notEmpty().exists(),
+  ],
+  controller.login
+);
+
+module.exports = router;
